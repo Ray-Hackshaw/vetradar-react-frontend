f28866536a444e23c49a3aac270d07ffdf9bcf72
36725950 <36725950+Ray-Hackshaw@users.noreply.github.com>
2020-09-21 20:53:44 +1200

    More style changes and changing header buttons.

--- src/App.js
+++ src/App.js
@@ -16,9 +16,9 @@ function App() {
     <div className="App">
       <header className="heading">
         <h1 className="title">
-          <span>Vet Radar Front End React Tests</span>
-          <button class="headerbtn border" onClick={() => switchPage(PAGE_PRODUCTS)}>Products</button>
-          <button class="headerbtn border" onClick={() => switchPage(PAGE_CART)}>My Cart</button>
+          <span className="border">Vet Radar Front End React Tests</span>
+          <a href="#" class="headerbtn border" onClick={() => switchPage(PAGE_PRODUCTS)}>Products</a>
+          <a href="#" class="headerbtn border" onClick={() => switchPage(PAGE_CART)}>My Cart</a>
         </h1>
       </header>
       {page === PAGE_PRODUCTS && (<Products cart={cart} setCart={setCart}/>)}
