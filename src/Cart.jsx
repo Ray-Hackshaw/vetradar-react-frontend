d310a559ef9c0a9a464dea52f3009e2a94f0505c
36725950 <36725950+Ray-Hackshaw@users.noreply.github.com>
2020-09-21 20:34:24 +1200

    Quantity tracking functionality working check.

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,6 +1,15 @@
 import React, {useState} from 'react';
 
 export default function Cart({cart, setCart}) {
+
+    const getTotalSum = () => {
+        return cart.reduce(
+          (sum, {price, quantity}) => sum + price * quantity,
+          0
+        );
+      };
+
+
     return (
         <>
           <div className="products">
@@ -8,9 +17,11 @@ export default function Cart({cart, setCart}) {
               <div className="product" key={index}>
                 <h3 className="names">{product.name}</h3>
                 <h4>${product.price}</h4>
+                <p>Quantity: {product.quantity}</p>
               </div>
             ))}
           </div>
+          <div className="total">Total: ${getTotalSum().toFixed(2)}</div>
         </>
       );
     }
