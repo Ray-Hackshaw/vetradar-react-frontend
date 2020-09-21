c707e7c803c47f371fc41eb6f248302981f64f1c
36725950 <36725950+Ray-Hackshaw@users.noreply.github.com>
2020-09-21 20:14:25 +1200

    Adding the products list into the App.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [products] = useState([
+    {
+    name: 'Sledgehammer',
+    price: 125.76
+    },
+    {
+    name: 'Axe',
+    price: 190.51
+    },
+    {
+    name: 'Bandsaw',
+    price: 562.14
+    },
+    {
+    name: 'Chisel',
+    price: 13.9
+    },
+    {
+    name: 'Hacksaw',
+    price: 19.45
+    }
+  ]);
   return (
     <div className="App">
       <h1>Vet Radar Front End React Tests</h1>
     </div>
-  );
-}
+    );
+  }
 
 export default App;
